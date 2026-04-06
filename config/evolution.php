b7c61646933b445b524a947de7819361c25036da
Abdelrahman Tarek <ar98tk@gmail.com>
2026-04-06 23:01:08 +0200

    update

--- config/evolution.php
+++ config/evolution.php
@@ -6,5 +6,5 @@ return [
     'token' => env('EVOLUTION_TOKEN', env('EVOLUTION_API_KEY', '')),
     'integration' => env('EVOLUTION_INTEGRATION', 'WHATSAPP-BAILEYS'),
     'webhook_base_url' => env('EVOLUTION_WEBHOOK_BASE_URL', env('APP_URL', '')),
-    'webhook_path' => env('EVOLUTION_WEBHOOK_PATH', '/webhook/whatsapp/{instance_key}'),
+    'webhook_path' => env('EVOLUTION_WEBHOOK_PATH', '/شحwebhook/whatsapp/{instance_key}'),
 ];
--- resources/views/admin/layout/components/head.blade.php
+++ resources/views/admin/layout/components/head.blade.php
@@ -3,7 +3,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}" />
 <title>@yield('title', 'Admin Dashboard')</title>
 
-<link rel="shortcut icon" href="{{ asset('assets/media/logos/favicon.ico') }}" />
+<link rel="shortcut icon" href="{{ asset('dpm-icon.png') }}" />
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 <link href="{{ asset('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
 <link href="{{ asset('assets/plugins/custom/datatables/datatables.bundle.css') }}" rel="stylesheet" type="text/css" />
