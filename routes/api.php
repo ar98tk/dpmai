302812d858dbcff3e76088c7e8e8322a4c130675
Abdelrahman Tarek <ar98tk@gmail.com>
2026-04-06 21:04:49 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::post('/webhook/whatsapp', [App\Http\Controllers\WhatsAppController::class, 'handle']);
-Route::post('/webhook/whatsapp', [App\Http\Controllers\WebhookController::class, 'handleWithoutInstanceKey']);
-Route::post('/webhook/whatsapp/{instance_key}', [App\Http\Controllers\WebhookController::class, 'handle']);
+Route::post('/webhook/whatsapp', [App\Http\Controllers\WhatsAppController::class, 'handle']);
